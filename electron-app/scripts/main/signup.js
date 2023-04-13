c6d1c35a863f5d68edb2edbc2b10e24cc4d8e38b
Ethan <ethanowen0707@gmail.com>
2023-04-13 22:53:05 +0100

    signup can now send info to express

--- electron-app/scripts/main/signup.js
+++ electron-app/scripts/main/signup.js
@@ -1,10 +1,36 @@
-// Return value meanings
-// 0 - Everything works
-// 1 - Password doesnt match
+const { net } = require('electron')
 
 // entry point function
 async function handleSignup(event, data){
-    console.log(data)
+
+    // Store data as a JSON object
+    var jsonData = {
+	email: data[0],
+	name: data[1],
+	password: data[2]
+    }
+
+    // Create a POST request to express server
+    const request = net.request({
+	method: 'POST',
+	url: 'http://localhost:3000/',
+    })
+    request.setHeader('Content-Type', 'application/json')
+
+    // On response just output what we recieved
+    request.on('response', (resp) => {
+	console.log(`STATUS: ${resp.statusCode}`)
+	console.log(`HEADERS: ${JSON.stringify(resp.headers)}`)
+	resp.on('data', (chunk) => {
+	    console.log(`BODY: ${chunk}`)
+	})
+    })
+
+    // Send JSON object to express server
+    request.write(JSON.stringify(jsonData))
+
+    request.end()
+
     return 'Recieved'
 }
 
