a61621b221091063908cb54efc95883f5751aa95
Ethan <ethanowen0707@gmail.com>
2023-04-16 01:53:51 +0100

    removed signup code as this has changed for server, will implement on monday

--- electron-app/scripts/main/signup.js
+++ electron-app/scripts/main/signup.js
@@ -3,33 +3,8 @@ const { net } = require('electron')
 // entry point function
 async function handleSignup(event, data){
 
-    // Store data as a JSON object
-    var jsonData = {
-	email: data[0],
-	name: data[1],
-	password: data[2]
-    }
-
-    // Create a POST request to express server
-    const request = net.request({
-	method: 'POST',
-	url: 'http://localhost:3000/signup',
-    })
-    request.setHeader('Content-Type', 'application/json')
-
-    // On response just output what we recieved
-    request.on('response', (resp) => {
-	console.log(`STATUS: ${resp.statusCode}`)
-	console.log(`HEADERS: ${JSON.stringify(resp.headers)}`)
-	resp.on('data', (chunk) => {
-	    console.log(`BODY: ${chunk}`)
-	})
-    })
-
-    // Send JSON object to express server
-    request.write(JSON.stringify(jsonData))
-
-    request.end()
+    // TODO 
+    console.log('signup pressed')
 
     return 'Recieved'
 }
