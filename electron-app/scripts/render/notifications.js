85185797a3a5286438ef04239341553b8f4d2dc4
Flynn Powell Williams <fpwtemp@outlook.com>
2023-04-17 13:42:57 +0100

    add ability to see if notifications have been read

--- electron-app/scripts/render/notifications.js
+++ electron-app/scripts/render/notifications.js
@@ -1,12 +1,24 @@
 window.onload = pageLoaded;
 const notificationsParagraph = document.getElementById('notifications')
+const notificationReadColour = "DodgerBlue";
+const notificationNotReadColour = "Tomato;"
 function pageLoaded(){
     console.log("page loaded.");
-    const notifcations = [{"Type":"normal", "Contents":"This is a test!"},{"Type":"normal", "Contents":"At some point this will display a bunch of notifications..."}];
+    const notifcations = [{"Type":"normal", "Contents":"Notification read test", "Read":true},{"Type":"normal", "Contents":"Notification not read test", "Read":false}];
     //Hard coding the notifications into the code, we will get this from sql at some point but for now...
-    let notif = ""; 
+    let notif = "<div class = \"content\">"; 
     for (var i = 0; i < notifcations.length; i++){
-        notif += notifcations[i].Contents + "<br>";
+        notif += addHtml(notifcations[i]);
     }
+
     notificationsParagraph.innerHTML = notif;
+}
+
+function addHtml(notification){
+    let toReturn = "";
+    let colour = "Tomato";
+    if (notification.Read) {colour = notificationReadColour; } else { colour = notificationNotReadColour; }
+    //notification += "<h2>" + notification.Title + "</h2>"
+    toReturn += '<p style="color:' + colour + ';">' + notification.Contents + "</p>"
+    return toReturn;
 }
