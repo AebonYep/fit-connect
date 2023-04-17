0ac13f7ee1c1246c9c30d49c170702f7f36e81c3
Ethan <ethanowen0707@gmail.com>
2023-04-17 11:51:26 +0100

    signup page linked with db

--- electron-app/scripts/main/signup.js
+++ electron-app/scripts/main/signup.js
@@ -6,6 +6,31 @@ async function handleSignup(event, data){
     // TODO 
     console.log('signup pressed')
 
+    let userData = {
+	email: data[0],
+	name: data[1],
+	password: data[2],
+    }
+
+    const request = net.request({
+	method: 'POST',
+	url: 'http://localhost:3000/users/signup',
+    })
+    request.setHeader('Content-Type', 'application/json')
+    // On response just output what we recieved
+    request.on('response', (resp) => {
+	console.log(`STATUS: ${resp.statusCode}`)
+	console.log(`HEADERS: ${JSON.stringify(resp.headers)}`)
+	resp.on('data', (chunk) => {
+	    console.log(`BODY: ${chunk}`)
+	})
+    })
+
+    // Send JSON object to express server
+    request.write(JSON.stringify(userData))
+
+    request.end()
+
     return 'Recieved'
 }
 
