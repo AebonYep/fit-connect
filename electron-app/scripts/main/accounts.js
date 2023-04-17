01201f59ffd8d085e7420462547bc3a5490a6f85
Ethan <ethanowen0707@gmail.com>
2023-04-17 13:23:11 +0100

    login linked with db

--- electron-app/scripts/main/accounts.js
+++ electron-app/scripts/main/accounts.js
@@ -34,4 +34,39 @@ async function handleSignup(event, data){
     return 'Recieved'
 }
 
+// entry point function
+async function handleLogin(event, data){
+
+    console.log('login pressed')
+
+    let userData = {
+	email: data[0],
+	password: data[1],
+    }
+
+    console.log(data)
+    console.log(userData)
+
+    const request = net.request({
+	method: 'POST',
+	url: 'http://localhost:3000/users/login',
+    })
+    request.setHeader('Content-Type', 'application/json')
+    // On response just output what we recieved
+    request.on('response', (resp) => {
+	console.log(`STATUS: ${resp.statusCode}`)
+	console.log(`HEADERS: ${JSON.stringify(resp.headers)}`)
+	resp.on('data', (chunk) => {
+	    console.log(`BODY: ${chunk}`)
+	})
+    })
+
+    // Send JSON object to express server
+    request.write(JSON.stringify(userData))
+    request.end()
+
+    return 'Recieved'
+}
+
+module.exports.handleLogin = handleLogin
 module.exports.handleSignup = handleSignup
