d030cf4b4453d30769251500e9f65ed2fd9c731b
snippy4 <75571417+snippy4@users.noreply.github.com>
2023-04-18 16:44:43 +0100

    Add files via upload

--- electron-app/scripts/render/popular.js
+++ electron-app/scripts/render/popular.js
@@ -1,32 +1,32 @@
-var posts = [{
-    "Title":"example 1",
-    "UserID":1, 
-    "Content":"example text 1",
-    "Tags":[
-       "#gains",
-       "#gym"
-    ]
- },{"Title":"Gainz2 electric boogaloo",
- "UserID":1,  
- "Content":"i have made sick gainz",
- "Tags":[
-    "#gains",
-    "#gym"
- ]}];
- 
- 
-   let text = "<div class = \"content\">"
-     posts.forEach(func);
-     text += "</div>"
- 
-     function func(post){
-       text += "<h2>" + post.Title + "</h2>"
-       /*text += "<img src="+ Images/example01.jpg +"> </img>" 
-       something similar to this should work*/
-       text += "<p>" + post.Content + "</p>"
-       text += "<small>" + post.Tags + "</small>"
-       text += "<br>" + "<small>" + post.UserID + "</small>"
-       text+= "<br><br>"
-     }
- 
-     document.getElementById("posts").innerHTML = text;
+
+var posts;
+
+
+async function grabdata() {
+   let text = "";
+   const response = await fetch("http://lxfarm08.csc.liv.ac.uk:3000/posts");
+   const jsonData = await response.json();
+   text += "<div class = \"content\">"
+   for (var i = 0; i<jsonData.length; i++) {
+      text += func(jsonData[i], text)
+  }
+   text += "</div>"
+   document.getElementById("posts").innerHTML = text;
+   return text
+}
+
+
+
+function func(post) {
+   let text = ""
+   text += "<h2>" + post.title + "</h2>"
+   /*text += "<img src="+ Images/example01.jpg +"> </img>" 
+   something similar to this should work*/
+   text += "<p>" + post.content + "</p>"
+   text += "<small>" + post.tags + "</small>"
+   //text += "<br>" + "<small>" + post.UserID + "</small>"
+   text += "<br><br>"
+   return text
+}
+let textresult = grabdata()
+document.getElementById("posts").innerHTML = textresult;
