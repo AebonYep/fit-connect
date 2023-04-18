69347d4b3a40fcc9baa583f64638369032229d91
JamesDearnley01 <sgjdearn@liverpool.ac.uk>
2023-04-18 12:38:37 +0100

    profile page placeholders

--- electron-app/scripts/render/profilePic.js
+++ electron-app/scripts/render/profilePic.js
@@ -1,23 +1,6 @@
-function test(){
-
-    alert("test")
-    
-    }
-
-$("#profileImage").click(function(e) {
-    $("#imageUpload").click();
-});
-
-function fasterPreview( uploader ) {
-    if ( uploader.files && uploader.files[0] ){
-          $('#profileImage').attr('src', 
-             window.URL.createObjectURL(uploader.files[0]) );
-    }
+function hello() {
+  alert('Hello');
 }
-
-$("#imageUpload").change(function(){
-    fasterPreview( this );
-});
 
 var profile = {
     "UserID":1,
