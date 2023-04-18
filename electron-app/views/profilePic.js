0f3266298be822621454e9cecb07eef8f684d9ae
nayfrater <nathanfrater.nf@gmail.com>
2023-04-18 11:36:21 +0100

    UI of feed page
    UI sorted js needs completing for both profile and feed pages as well as profile page html fixing

--- electron-app/views/profilePic.js
+++ electron-app/views/profilePic.js
@@ -1,20 +1,23 @@
-$("#profileImage").click(function(e) {
-    $("#imageUpload").click();
-});
-
-function fasterPreview( uploader ) {
-    if ( uploader.files && uploader.files[0] ){
-          $('#profileImage').attr('src', 
-             window.URL.createObjectURL(uploader.files[0]) );
-    }
-}
-
-$("#imageUpload").change(function(){
-    fasterPreview( this );
-});
-
-function test(){
-
-alert("test")
-
-}
+var profile = {
+    "UserID":1,
+    "Bio": "Example bio", 
+    //"Images:[] -- just fill with example for images for now should be filled with images uploaded by user"
+ };
+ 
+ 
+   /*let text = "<div class = \"content\">"
+     posts.forEach(func);
+     text += "</div>"
+ 
+     function func(post){
+       text += "<h2>" + post.Title + "</h2>"
+       /*text += "<img src="+ Images/example01.jpg +"> </img>" 
+       something similar to this should work
+       text += "<p>" + post.Content + "</p>"
+       text += "<small>" + post.Tags + "</small>"
+       text += "<br>" + "<small>" + post.UserID + "</small>"
+       text+= "<br><br>"
+     }
+ 
+     document.getElementById("profile").innerHTML = text;
+     */
