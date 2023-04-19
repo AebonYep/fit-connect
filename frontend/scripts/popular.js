f6d575ef644adb57d51d189aef1ae806c8732b2a
Ethan <ethanowen0707@gmail.com>
2023-04-19 13:33:08 +0100

    fixed js to work with server.js

--- frontend/scripts/popular.js
+++ frontend/scripts/popular.js
@@ -1,3 +1,5 @@
+const address = localStorage.getItem("address")
+const port = localStorage.getItem("port")
 
 var posts;
 
@@ -5,7 +7,7 @@ var posts;
 async function grabdata() {
    let text = "";
    // CHANGE LINK TO VARIABLE THAT IS GLOBAL IN CODEBASE
-   const response = await fetch("http://lxfarm08.csc.liv.ac.uk:3000/posts");
+   const response = await fetch(`${address}:${port}/posts`);
    const jsonData = await response.json();
    text += "<div class = \"content\">"
    for (var i = 0; i<jsonData.length; i++) {
