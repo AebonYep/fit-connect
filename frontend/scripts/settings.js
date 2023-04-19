68d5d07f69f003c258c88dd914bdf247f870d2d5
Ethan <ethanowen0707@gmail.com>
2023-04-19 13:17:49 +0100

    can now delete accounts via settings

--- frontend/scripts/settings.js
+++ frontend/scripts/settings.js
@@ -2,19 +2,28 @@ const deletePasswordInput = document.getElementById('delete-password-input')
 const deleteAccountBtn = document.getElementById('delete-account-btn')
 
 deleteAccountBtn.addEventListener('click', async (event) => {
-    event.preventDefault()
+	event.preventDefault()
+	console.log("account delete button pressed")
 
-    let jsonData = {
-	password: deletePasswordInput.value
-    }
+	let jsonData = {
+		userID: localStorage.getItem("userID"),
+		password: deletePasswordInput.value
+	}
 
-    let response = await fetch("http://localhost:3000/users/delete",{
-	method: "POST",
-	mode: "cors",
-	headers: {
-	    "Content-Type": "application/json"
-	},
-	body: JSON.stringify(jsonData)
-    })
-    console.log(response)
+	let response = await fetch("http://localhost:3000/users/delete", {
+		method: "POST",
+		mode: "cors",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(jsonData)
+	})
+	if(response.status === 200){
+		localStorage.setItem("userID", null)
+		window.location.href="../views/index.html"
+	}
+	else if(response.status === 401){
+		alert("incorrect password")
+	}
+	console.log(response.status)
 })
