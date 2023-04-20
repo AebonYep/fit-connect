62ee8aeb798c845441c853aa738e24c95288ead9
snippy4 <75571417+snippy4@users.noreply.github.com>
2023-04-20 11:59:16 +0100

    Add files via upload

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -2,28 +2,62 @@ const express = require('express')
 const router = express.Router()
 const con = require('../mysqlConnection')
 
-function simpleGet(query, callback) {
-	con.query(query, (err, result) => {
-		if (err) {
-			return callback(500)
-		}
-		if (result.length > 0) {
-			return callback(result)
-		}
-		else {
-			return callback(404)
-		}
-	})
+function simpleGet(query, callback){
+    con.query(query, (err, result) => {
+	if (err) {
+	    throw err
+	    return callback(500)
+	}
+	if(result.length > 0){
+	    return callback(result)
+	}
+	else{
+	    return callback(404)
+	}
+    })
 }
 
 router.get('/', (req, res) => {
 
-    console.log('ok')
-	let getUsersQuery = `SELECT * FROM user_messages`
+    let getPostsQuery = `SELECT * FROM user_messages ORDER BY sent`
 
-	simpleGet(getUsersQuery, (result) => {
-		res.send(result)
-	})
+    simpleGet(getPostsQuery, (result) => {
+	res.send(result)
+    })
+
 })
 
-module.exports = router
+
+
+router.get('/id=:messagewith&nextid=:sentid', (req, res) => {
+    let { messagewith, sentid } = req.params
+    
+
+    let getPostsQuery = `SELECT * FROM user_messages WHERE sender_id = ${sentid} OR sentto_id = ${sentid} ORDER BY sent`
+
+    simpleGet(getPostsQuery, (result) => {
+	res.send(result)
+    })
+
+})
+
+router.post('/create', (req, res) => {
+    let userid = req.body.userid
+    let otheruserid = req.body.otheruserid
+    let content = req.body.content
+    let createPostQuery = `INSERT INTO user_messages (sender_id, sentto_id, content, sent) values(${userid}, ${otheruserid}, '${content}', CURRENT_TIMESTAMP)`
+
+    con.query(createPostQuery, (err, result) => {
+	if (err) {
+	    res.sendStatus(500)
+	    throw err
+	}
+
+	res.sendStatus(200)
+    })
+
+})
+
+
+
+module.exports = router;
